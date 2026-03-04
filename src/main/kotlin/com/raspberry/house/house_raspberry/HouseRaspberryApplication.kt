2e5c631f052ef9ce66a8f1f139313d8d8ac1ad2a
RafaelFerreiraNunes <rafaelguarulhos@hotmail.com>
2026-03-03 22:35:30 -0300

    feat: configs iniciais proj house

---
+++ src/main/kotlin/com/raspberry/house/house_raspberry/HouseRaspberryApplication.kt
@@ -0,0 +1,11 @@
+package com.raspberry.house.house_raspberry
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HouseRaspberryApplication
+
+fun main(args: Array<String>) {
+    runApplication<HouseRaspberryApplication>(*args)
+}
---
+++ src/test/kotlin/com/raspberry/house/house_raspberry/HouseRaspberryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.raspberry.house.house_raspberry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HouseRaspberryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
